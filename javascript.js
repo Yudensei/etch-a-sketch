b301af7ec034ecba553a42b799cbf97f384f4f33
Yudensei <yudensei.140@hotmail.com>
2025-05-16 01:36:00 -0300

    Add functional 'Erase' button

--- javascript.js
+++ javascript.js
@@ -4,13 +4,15 @@ div.setAttribute("class", "square");
 const container = document.querySelector("#container");
 container.addEventListener("mouseover", (e) => makeTrail(e.target));
 
+const erase = document.querySelector("#prompt");
+erase.addEventListener("click", promptErase)
+
 makeGrid(16);
 
 
-
-
 function makeGrid(sideLength) {
-    for (let i = 0; i < sideLength * sideLength; i++) {
+    container.textContent = '';
+    for (let i = 0; i < sideLength ** 2; i++) {
         const square = div.cloneNode();
         square.style.flex = `1 0 ${1/sideLength * 100}%`
         container.appendChild(square);
@@ -20,4 +22,15 @@ function makeGrid(sideLength) {
 function makeTrail(target) {
     if (target.id === "container") return;
     target.style.backgroundColor = "black";
-} 
+}
+
+function promptErase() {
+    const choice = +prompt("This will erase your grid and create a new one.\nWhat size should be the new grid?", 16)
+    if (isNaN(choice)) {
+        alert("Please enter a valid number")
+    } else if (choice > 100) {
+        alert("Please enter a smaller number (max. 100)")
+    } else {
+        makeGrid(choice)
+    }
+}
